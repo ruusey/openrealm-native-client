6c5d14a24c348d8357717725c71a45af604bb955
Robert Usey <ruusey@gmail.com>
2026-07-07 21:19:04 -0400

    PortalModel: dungeonId target

--- src/main/java/com/openrealm/game/model/PortalModel.java
+++ src/main/java/com/openrealm/game/model/PortalModel.java
@@ -8,8 +8,9 @@ import lombok.EqualsAndHashCode;
 public class PortalModel extends SpriteModel {
     private int portalId;
     private String portalName;
-    private int mapId;
-    // Graph-based dungeon system: the target node this portal leads to
+    // A portal routes to EITHER a map (mapId) OR a dungeon (dungeonId); unset is -1.
+    private int mapId = -1;
+    private int dungeonId = -1;
     private String targetNodeId;
     // Static portal placement: world position (pixels) when placed on a map
     private float x;
